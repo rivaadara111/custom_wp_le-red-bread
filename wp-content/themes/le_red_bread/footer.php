1b1f92a35abd780b6f01edf8f70c7e663414c35d
Riva Adara Ridley <rivaridley1@Riva-Ridleys-MacBook-Pro-2.local>
2016-03-04 16:51:59 -0800

    Friday end of day.

--- wp-content/themes/le_red_bread/footer.php
+++ wp-content/themes/le_red_bread/footer.php
@@ -10,8 +10,25 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="footer-blocks-container">
+					<div class="footer-blocks-item">
+						<h2>contact</h2>
+						<p><i class="fa fa-envelope"></i>info@leredbread.com</p>
+						<p>(555) 343 456 7891</p>
+						<p> <i class="fa fa-facebook-square"></i><i class="fa fa-twitter-square"></i><i class="fa fa-google-plus-square"></i></p>
+					</div>
+					<div class="footer-blocks-item">
+						<img src="<?php bloginfo('template_directory'); ?>/images/lrb-logo-white.svg" class="logo" alt="Le Red Bread logo">
+					</div>
+					<div class="footer-blocks-item">
+						<h2>business hours</h2>
+						<p><span>Monday-Friday:</span> 9am to 5pm</p>
+						<p><span>Saturday:</span> 10am to 2pm</p>
+						<p><span>Sunday:</span> Closed</p>
+					</div>
+				</div>
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				 <p> Copyright &copy; 2016 RED Academy</p>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
--- wp-content/themes/le_red_bread/header.php
+++ wp-content/themes/le_red_bread/header.php
@@ -10,6 +10,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
@@ -26,17 +27,40 @@
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<img src="<?php bloginfo('template_directory'); ?>/images/lrb-logo.svg" class="logo" alt="Le Red Bread logo">
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<div class="social">
+							<ul>
+									<li>
+										<a href="www.facebook.com"><i class="fa fa-facebook"></i>
+										</a>
+									</li>
+									<li>
+											<a href="wwww.twitter.com"><i class="fa fa-twitter"></i>
+									 </a>
+									</li>
+									<li>
+											<a href="www.google.ca"><i class="fa fa-google-plus"></i>
+									 </a>
+									</li>
+									<li>
+											<a href="#"><i class="fa fa-envelope"></i>
+									 </a>
+									</li>
+							</ul>
+					</div>
 				</div><!-- .site-branding -->
 
-				<form role="search" method="get" id="searchform" action="http://example.com/">
+
+		<form role="search" method="get" id="searchform" action="http://example.com/">
 					<nav id="site-navigation" class="main-navigation" role="navigation">
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					</nav><!-- #site-navigation -->
-	    <div class ="search-bar">
+
+	  	<div class ="search-bar">
 	        <label class="screen-reader-text" for="s">' . __('Search for:') . '</label>
-	        <input type="text" value="" name="s" id="s" />
-	        <input type="submit" id="searchsubmit" value="Suche" />
+	        <input type="text" value="" placeholder="SEARCH ..." name="s" id="s" />
+<div class="searchicon"><i class="fa fa-search"></i></div>
+
 	    </div>
 	</form>
 			</header><!-- #masthead -->
