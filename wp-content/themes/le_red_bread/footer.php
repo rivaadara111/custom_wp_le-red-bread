1f1de5c8379e4fd0f6e385646e2921fc1e6c9236
Riva Adara Ridley <rivaridley1@Riva-Ridleys-MacBook-Pro-2.local>
2016-03-06 16:10:43 -0800

    Blog page.

--- wp-content/themes/le_red_bread/footer.php
+++ wp-content/themes/le_red_bread/footer.php
@@ -13,16 +13,16 @@
 				<div class="footer-blocks-container">
 					<div class="footer-blocks-item">
 						<h2>contact</h2>
-						<p><i class="fa fa-envelope"></i>info@leredbread.com</p>
-						<p>(555) 343 456 7891</p>
+						<p><i class="fa fa-envelope"></i><a href="mailto:info@leredbread.com">info@leredbread.com</a></p>
+						<p><i class="fa fa-phone"></i><a href="tel:555-555-5555">(555) 343 456 7891</a></p>
 						<p> <i class="fa fa-facebook-square"></i><i class="fa fa-twitter-square"></i><i class="fa fa-google-plus-square"></i></p>
 					</div>
 					<div class="footer-blocks-item">
 						<img src="<?php bloginfo('template_directory'); ?>/images/lrb-logo-white.svg" class="logo" alt="Le Red Bread logo">
 					</div>
-					<div class="footer-blocks-item">
-						<h2>business hours</h2>
-						<p><span>Monday-Friday:</span> 9am to 5pm</p>
+					<div class="footer-blocks-item" >
+						<h2 class="business-hours">business hours</h2>
+						<p class="business-hours"><span>Monday-Friday:</span> 9am to 5pm</p>
 						<p><span>Saturday:</span> 10am to 2pm</p>
 						<p><span>Sunday:</span> Closed</p>
 					</div>
--- wp-content/themes/le_red_bread/template-parts/content.php
+++ wp-content/themes/le_red_bread/template-parts/content.php
@@ -12,9 +12,9 @@
 		<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
-
+		<div class="entry-title">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
+		</div> <!--entry-title -->
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
